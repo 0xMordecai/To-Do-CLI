5d3df52709c6614b45ded715b9fdaa584d2c878b
0xMordecai <sir.lahrach.mohamed@gmail.com>
2025-09-15 23:02:02 +0100

    add

--- todo.go
+++ todo.go
@@ -1,1 +1,11 @@
 package todo
+
+import "time"
+
+// item struct represents a ToDo item
+type item struct {
+	Task        string
+	Done        bool
+	CreatedAt   time.Time
+	CompletedAt time.Time
+}
