d8849432b4972a79231ce27c844cfcf709ee4469
0xMordecai <sir.lahrach.mohamed@gmail.com>
2025-09-16 23:18:43 +0100

    add

--- todo.go
+++ todo.go
@@ -50,4 +50,7 @@ func (l *List) Complete(i int) error {
 // Delete method deletes a ToDo item from the list
 func (l *List) Delete(i int) error {
 	ls := *l
+	if i<=0 || i>len(ls){
+		return fmt.Errorf("Item %d does not exist",i)
+	}
 }
