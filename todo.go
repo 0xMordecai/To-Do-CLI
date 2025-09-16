2ead824b64b95b66fb554e93d063c999fbbafea1
0xMordecai <sir.lahrach.mohamed@gmail.com>
2025-09-16 22:29:38 +0100

    add

--- todo.go
+++ todo.go
@@ -14,7 +14,9 @@ type item struct {
 type List []item
 
 // Add creates a new todo item and appends it to the list
-// --> (l *List) is a reciever that will attach Add() method to List type
+// --> (l *List) is a reciever that will attach Add() method to List type ;
+// You can use any valid Go identifier,
+// but it’s common to use the first letter of the type name as the identifier—in this case,l (lowercase L) for List
 func (l *List) Add(task string) {
 	t := item{
 		Task:        task,
