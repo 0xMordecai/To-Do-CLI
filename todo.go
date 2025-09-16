bcb013bd32fcc1f8856e3bb5264014ae7aac597c
0xMordecai <sir.lahrach.mohamed@gmail.com>
2025-09-16 23:06:22 +0100

    add

--- todo.go
+++ todo.go
@@ -1,6 +1,9 @@
 package todo
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 // item struct represents a ToDo item
 type item struct {
@@ -31,3 +34,15 @@ func (l *List) Add(task string) {
 
 // Complete method marks a ToDo item as completed by
 // setting Done = true and CompletedAt to the current time
+func (l *List) Complete(i int) error {
+	ls := *l
+	if i <= 0 || i > len(ls) {
+		return fmt.Errorf("Item %d does not exist", i)
+	}
+
+	// Adjusting index for 0 based index
+	ls[i-1].Done = true
+	ls[i-1].CompletedAt = time.Now()
+
+	return nil
+}
