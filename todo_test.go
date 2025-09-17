d86587478061fa310fc153f95f6d34223df3abf3
0xMordecai <sir.lahrach.mohamed@gmail.com>
2025-09-17 21:40:51 +0100

    add

--- todo_test.go
+++ todo_test.go
@@ -2,9 +2,17 @@ package todo_test
 
 import (
 	"testing"
+
+	todo "github.com/0xMordecai/To-Do-CLI.git"
 )
 
 // TestAdd tests the Add method of the List type
 func TestAdd(t *testing.T) {
+	l := todo.List{}
 
+	taskName := "NAw Task"
+	l.Add(taskName)
+	if l[0].Task != taskName {
+		t.Errorf("Expected %q, got %q instead", taskName, l[0].Task)
+	}
 }
