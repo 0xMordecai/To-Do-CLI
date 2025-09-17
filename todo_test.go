627320ec70eff9f22be506f47f40297e6b4cd53f
0xMordecai <sir.lahrach.mohamed@gmail.com>
2025-09-17 22:12:35 +0100

    add

--- todo_test.go
+++ todo_test.go
@@ -16,3 +16,15 @@ func TestAdd(t *testing.T) {
 		t.Errorf("Expected %q, got %q instead", taskName, l[0].Task)
 	}
 }
+
+func TestComplete(t *testing.T) {
+	l := todo.List{}
+
+	taskName := "New Task"
+	l.Add(taskName)
+
+	if l[0].Task != taskName {
+		t.Errorf("Expected %q, got %q instead", taskName, l[0].Task)
+	}
+
+}
