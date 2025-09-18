10ae6636d8c9060555433ccc2a52808d9e09f922
0xMordecai <sir.lahrach.mohamed@gmail.com>
2025-09-18 22:26:09 +0100

    add

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -19,4 +19,15 @@ func main() {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
+
+	// Decide what to-do based on the number of arguments provided
+	switch {
+	// For no extra arguments, print the list
+	case len(os.Args) == 1:
+		// List current to do items
+		for _, item := range *l {
+			fmt.Println(item)
+		}
+
+	}
 }
