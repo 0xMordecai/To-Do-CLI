b3e3313032487da06f6c43dbe488e4673b60bfdf
0xMordecai <sir.lahrach.mohamed@gmail.com>
2025-09-18 22:37:09 +0100

    add

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	todo "github.com/0xMordecai/To-Do-CLI"
 )
@@ -28,10 +29,20 @@ func main() {
 		for _, item := range *l {
 			fmt.Println(item)
 		}
-	default:
-		// Concatenate all arguments with a space
-	}
 	// Concatenate all provided arguments with a space and
 	// add to the list as an item
+	default:
+		// Concatenate all arguments with a space
+		item := strings.Join(os.Args[1:], "")
+
+		// Add the task
+		l.Add(item)
+
+		// Save the new list
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	}
 
 }
