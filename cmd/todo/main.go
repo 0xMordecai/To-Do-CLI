9340248dab6aba5f5233c821348078d54541259c
0xMordecai <sir.lahrach.mohamed@gmail.com>
2025-09-18 21:53:07 +0100

    add

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,6 +1,11 @@
 package main
 
-import todo "github.com/0xMordecai/To-Do-CLI"
+import (
+	"fmt"
+	"os"
+
+	todo "github.com/0xMordecai/To-Do-CLI"
+)
 
 // Hardcoding the file name
 const todoFileName = ".todo.json"
@@ -10,5 +15,8 @@ func main() {
 	l := &todo.List{}
 
 	// Use the Get() method to read to-do items from file
-
+	if err := l.Get(todoFileName); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
