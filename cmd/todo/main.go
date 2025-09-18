ee42f50feed688309991fb01d2800a061ba4f05e
0xMordecai <sir.lahrach.mohamed@gmail.com>
2025-09-18 21:32:25 +0100

    add

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,8 +1,12 @@
 package main
 
+import todo "github.com/0xMordecai/To-Do-CLI"
+
 // Hardcoding the file name
 const todoFileName = ".todo.json"
 
 func main() {
+	// Define an items list
+	l := &todo.List{}
 
 }
