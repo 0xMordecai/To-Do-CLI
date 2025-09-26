984b933280c62eea259c9f549254ea90226c9ce2
0xMordecai <sir.lahrach.mohamed@gmail.com>
2025-09-26 21:54:12 +0100

    update list switch statement to select actions based on the provided flag(list, complete, task)

--- cmd/todo/main_test.go
+++ cmd/todo/main_test.go
@@ -6,7 +6,6 @@ import (
 	"os/exec"
 	"path/filepath"
 	"runtime"
-	"strings"
 	"testing"
 )
 
@@ -56,7 +55,7 @@ func TestTodoCLI(t *testing.T) {
 	cmdPath := filepath.Join(dir, binName)
 
 	t.Run("AddNewTask", func(t *testing.T) {
-		cmd := exec.Command(cmdPath, strings.Split(task, " ")...)
+		cmd := exec.Command(cmdPath, "-list")
 
 		if err := cmd.Run(); err != nil {
 			t.Fatal(err)
