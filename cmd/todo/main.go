11c3ddd1139e8bc62143cc2c012abc8249ac4dc0
0xMordecai <sir.lahrach.mohamed@gmail.com>
2025-09-26 20:51:58 +0100

    Add

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -4,7 +4,6 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"strings"
 
 	todo "github.com/0xMordecai/To-Do-CLI"
 )
@@ -27,30 +26,30 @@ func main() {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
+	// todo : V1
+	// // Decide what to-do based on the number of arguments provided
+	// switch {
+	// // For no extra arguments, print the list
+	// case len(os.Args) == 1:
+	// 	// List current to do items
+	// 	for _, item := range *l {
+	// 		fmt.Println(item.Task) // --> to print only Task
+	// 		//	fmt.Println(item)  // --> to print ALL info :=> fixed BUG
+	// 	}
+	// // Concatenate all provided arguments with a space and
+	// // add to the list as an item
+	// default:
+	// 	// Concatenate all arguments with a space
+	// 	item := strings.Join(os.Args[1:], " ")
 
-	// Decide what to-do based on the number of arguments provided
-	switch {
-	// For no extra arguments, print the list
-	case len(os.Args) == 1:
-		// List current to do items
-		for _, item := range *l {
-			fmt.Println(item.Task) // --> to print only Task
-			//	fmt.Println(item)  // --> to print ALL info :=> fixed BUG
-		}
-	// Concatenate all provided arguments with a space and
-	// add to the list as an item
-	default:
-		// Concatenate all arguments with a space
-		item := strings.Join(os.Args[1:], " ")
+	// 	// Add the task
+	// 	l.Add(item)
 
-		// Add the task
-		l.Add(item)
-
-		// Save the new list
-		if err := l.Save(todoFileName); err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(1)
-		}
-	}
+	// 	// Save the new list
+	// 	if err := l.Save(todoFileName); err != nil {
+	// 		fmt.Fprintln(os.Stderr, err)
+	// 		os.Exit(1)
+	// 	}
+	// }
 
 }
