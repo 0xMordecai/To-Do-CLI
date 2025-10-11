7d87a763115aee877b1167195f982ef22ec5604f
0xMordecai <sir.lahrach.mohamed@gmail.com>
2025-10-11 22:06:40 +0100

    update

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -35,31 +35,6 @@ func main() {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-	// todo : V1
-	// // Decide what to-do based on the number of arguments provided
-	// switch {
-	// // For no extra arguments, print the list
-	// case len(os.Args) == 1:
-	// 	// List current to do items
-	// 	for _, item := range *l {
-	// 		fmt.Println(item.Task) // --> to print only Task
-	// 		//	fmt.Println(item)  // --> to print ALL info :=> fixed BUG
-	// 	}
-	// // Concatenate all provided arguments with a space and
-	// // add to the list as an item
-	// default:
-	// 	// Concatenate all arguments with a space
-	// 	item := strings.Join(os.Args[1:], " ")
-
-	// 	// Add the task
-	// 	l.Add(item)
-
-	// 	// Save the new list
-	// 	if err := l.Save(todoFileName); err != nil {
-	// 		fmt.Fprintln(os.Stderr, err)
-	// 		os.Exit(1)
-	// 	}
-	// }
 
 	// Decide what to do based on the provided flags
 	switch {
