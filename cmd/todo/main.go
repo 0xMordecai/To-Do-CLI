8cf4b4687c9a284d30499cdfa37c23f852ede496
0xMordecai <sir.lahrach.mohamed@gmail.com>
2025-10-13 20:41:56 +0100

    update

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -80,6 +80,9 @@ func main() {
 
 }
 
+// The getTask() function accepts as input the parameter r of type io.Reader interface
+// and the parameter args, which consists of zero or more values of type string,
+// represented by the ... operator preceding the parameter type
 func getTask(r io.Reader, args ...string) (string, error) {
 	if len(args) > 0 {
 		return strings.Join(args, ""), nil
