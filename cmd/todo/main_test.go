efbc8515c9ef824d51a60a9f62b416e888fe3d7d
0xMordecai <sir.lahrach.mohamed@gmail.com>
2025-10-22 20:35:15 +0100

    update

--- cmd/todo/main_test.go
+++ cmd/todo/main_test.go
@@ -94,8 +94,9 @@ func TestTodoCLI(t *testing.T) {
 	// 	Exercice 5: Include test cases for the remaining options, such as -complete.
 	taskIndex := "2"
 	t.Run("CompleteTasks", func(t *testing.T) {
-		// mark the selected task with complete (X)
+		//	mark the selected task with complete (X)
 		cmd1 := exec.Command(cmdPath, "-complete", taskIndex)
+		//	list the tasks
 		cmd2 := exec.Command(cmdPath, "-list")
 		if err := cmd1.Run(); err != nil {
 			t.Fatal(err)
